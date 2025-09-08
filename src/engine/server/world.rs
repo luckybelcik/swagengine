737ae55a5c60e2d9725881861825b9aef53e8604
luckybelcik <luckybelcik@gmail.com>
2025-09-08 19:11:38 +0200

    Added a function that returns a basic dimension

--- src/engine/server/world.rs
+++ src/engine/server/world.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 use dashmap::DashMap;
+use hecs::World;
 
 use crate::engine::{common::{IVec2, Vec2}, components::alive::{AliveTask, AliveTaskKey, EntityID, PlayerID}, server::chunk::Chunk};
 
 pub struct Dimension {
-    name: String,
-    size: Vec2,
+    pub name: String,
+    size: IVec2,
     ecs_world: hecs::World,
     chunks: HashMap<IVec2, Chunk>,
     players: HashMap<PlayerID, hecs::Entity>,
@@ -15,4 +16,15 @@ pub struct Dimension {
 }
 
 impl Dimension {
+    pub fn new_basic_dimension() -> Dimension {
+        return Dimension { 
+            name: ("basic_dimension".to_string()),
+            size: (IVec2 { x: (10), y: (10) }),
+            ecs_world: (World::new()),
+            chunks: (HashMap::new()),
+            players: (HashMap::new()),
+            player_tasks: (DashMap::new()),
+            entities: (HashMap::new()),
+            entity_tasks: (DashMap::new()) }
+    }
 }
