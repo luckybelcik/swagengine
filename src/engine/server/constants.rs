5c0ef395f1ed983776fbc5e952f5d89757927d87
luckybelcik <luckybelcik@gmail.com>
2025-09-12 21:25:34 +0200

    changed a comment in constants.rs

--- src/engine/server/constants.rs
+++ src/engine/server/constants.rs
@@ -1,3 +1,5 @@
-// chunk size has to be a multiple of 2
-pub const CHUNK_SIZE: usize = 64;
+// stay within a size of 64 because otherwise the stack might overflow
+// thats because while constructed, the chunk is stored on the stack
+// it is only put on the heap once it enters the hashmap
+pub const CHUNK_SIZE: usize = 63;
 pub const CHUNK_BLOCK_COUNT: usize = CHUNK_SIZE * CHUNK_SIZE;
