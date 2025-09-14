6533d4c72c9d78f174e00ecba0a1d7e1c2e8ff23
luckybelcik <luckybelcik@gmail.com>
2025-09-14 09:35:05 +0200

    Oops the chunk size should not be 63

--- src/engine/server/constants.rs
+++ src/engine/server/constants.rs
@@ -1,5 +1,5 @@
 // stay within a size of 64 because otherwise the stack might overflow
 // thats because while constructed, the chunk is stored on the stack
 // it is only put on the heap once it enters the hashmap
-pub const CHUNK_SIZE: usize = 63;
+pub const CHUNK_SIZE: usize = 64;
 pub const CHUNK_BLOCK_COUNT: usize = CHUNK_SIZE * CHUNK_SIZE;
