7de1eee39551d24e0a0b94ed5bcda7180f58ffcb
luckybelcik <luckybelcik@gmail.com>
2025-09-14 16:10:37 +0200

    Switched the name "App" to "Client" (hopefully) everywhere

--- src/engine/client/commands.rs
+++ src/engine/client/commands.rs
@@ -8,8 +8,8 @@ pub fn create_client_commands() -> Vec<DebugCommand> {
         aliases: &["fps"],
         description: "Prints the average framerate. Sampled since program start, or last resetfps execution.",
         execute: |dependency, _args| {
-            if let CommandDependency::App(app) = dependency {
-                println!("Average FPS: {:.2}", app.time.average_fps());
+            if let CommandDependency::Client(client) = dependency {
+                println!("Average FPS: {:.2}", client.time.average_fps());
             }
         },
         command_environment: CommandEnvironment::Client,
@@ -20,10 +20,10 @@ pub fn create_client_commands() -> Vec<DebugCommand> {
         aliases: &["setfps", "setframerate"],
         description: "Sets the target framerate.",
         execute: |dependency, _args| {
-            if let CommandDependency::App(app) = dependency {
+            if let CommandDependency::Client(client) = dependency {
                 if let Some(arg) = _args.first() {
                     if let Ok(new_cap) = arg.parse::<u32>() {
-                        app.app_config.frame_cap = new_cap;
+                        client.client_config.frame_cap = new_cap;
                         println!("Frame cap set to {new_cap}");
                     } else {
                         error_wrong_type();
@@ -41,8 +41,8 @@ pub fn create_client_commands() -> Vec<DebugCommand> {
         aliases: &["rfps"],
         description: "Resets the average framerate sample frames.",
         execute: |dependency, _args| {
-            if let CommandDependency::App(app) = dependency {
-                app.time.reset_average_fps();
+            if let CommandDependency::Client(client) = dependency {
+                client.time.reset_average_fps();
                 println!("Reset average framerate sample frames")
             }
         },
@@ -54,9 +54,9 @@ pub fn create_client_commands() -> Vec<DebugCommand> {
         aliases: &["tvsync"],
         description: "Toggles VSync.",
         execute: |dependency, _args| {
-            if let CommandDependency::App(app) = dependency {
-                app.app_config.vsync = !app.app_config.vsync;
-                let bool = app.app_config.vsync;
+            if let CommandDependency::Client(client) = dependency {
+                client.client_config.vsync = !client.client_config.vsync;
+                let bool = client.client_config.vsync;
                 println!("VSync toggled to {bool}");
             }
         },
--- src/engine/util.rs
+++ src/engine/util.rs
@@ -1,9 +1,9 @@
-pub struct AppConfig {
+pub struct ClientConfig {
     pub frame_cap: u32,
     pub vsync: bool
 }
 
-impl Default for AppConfig {
+impl Default for ClientConfig {
     fn default() -> Self {
         Self { 
             frame_cap: 60, 
