c639656b58d1c331ff7083498e60ac66ec3171c4
luckybelcik <luckybelcik@gmail.com>
2025-09-19 20:58:33 +0200

    Fixed stack overflow due to chunk packet decoding by lowering the chunk size to 32x32

--- src/engine/server/constants.rs
+++ src/engine/server/constants.rs
@@ -1,7 +1,9 @@
-// stay within a size of 64 because otherwise the stack might overflow
+// stay within a size of 32 because otherwise the stack might overflow
 // thats because while constructed, the chunk is stored on the stack
 // it is only put on the heap once it enters the hashmap
-pub const CHUNK_SIZE: u8 = 64;
+// previously the limit was 64 but decoding packets made the stack overflow
+// oops?
+pub const CHUNK_SIZE: u8 = 32;
 pub const CHUNK_BLOCK_COUNT: u16 = CHUNK_SIZE as u16 * CHUNK_SIZE as u16;
 
 pub const TICK_RATE: u64 = 60;
